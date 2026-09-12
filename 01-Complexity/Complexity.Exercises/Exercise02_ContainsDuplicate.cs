0ae29caec75b46d61f5a372edf8ba08111aa56ba
Jonathan Lopes <jdmlopes97@gmail.com>
2026-09-12 00:04:16 -0300

    2nd exercise solved

--- 01-Complexity/Complexity.Exercises/Exercise02_ContainsDuplicate.cs
+++ 01-Complexity/Complexity.Exercises/Exercise02_ContainsDuplicate.cs
@@ -8,6 +8,15 @@ public static class ContainsDuplicate
 {
     public static bool HasDuplicate(int[] numbers)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(numbers);
+        if(numbers.Length < 2)
+            return false;
+        HashSet<int> numberSet = [];
+        foreach(int n in numbers)
+        {
+            if(numberSet.Add(n) == false)
+                return true;
+        }
+        return false;
     }
 }
