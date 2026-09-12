485d5c4988d7d5a2a43cb0556734d20d065c6418
Jonathan Lopes <jdmlopes97@gmail.com>
2026-09-11 23:46:03 -0300

    1st exercise solved

--- 01-Complexity/Complexity.Exercises/Exercise01_CountPairsWithSum.cs
+++ 01-Complexity/Complexity.Exercises/Exercise01_CountPairsWithSum.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace DsaPractice.Complexity;
 
 /// <summary>
@@ -8,6 +10,46 @@ public static class CountPairsWithSum
 {
     public static long Count(int[] numbers, int target)
     {
-        throw new NotImplementedException();
+        if(numbers is null)
+            throw new ArgumentNullException("Invalid input");
+        // create a dictionary to store seen numbers
+        // key = the number
+        // value = how many times it appears 
+        Dictionary<long, int> seen = new();
+        //count how many pairs sum up to the targer value
+        long count = 0;
+        foreach(int x in numbers)
+        {
+            //this is the value that summed up with the current number would give the target
+            long remaining = target - x;
+            //checks if the remaining value was seen before
+            if(seen.TryGetValue(remaining, out int currentCount))
+            {
+                // increment the count by the count of the times the value was seen
+                count += currentCount;
+            }
+            //adds or increments the current number to the dictionary
+            seen[x] = seen.GetValueOrDefault(x) + 1;
+        }
+
+        
+        return count;
     }
+
+    /* --- O(n²) SOLUTION 
+    public static long Count(int[] numbers, int target)
+    {
+        if(numbers is null)
+            throw new ArgumentException("Invalid input");
+        long count = 0;
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            for(int j = 0; j <  numbers.Length; j++)
+            {
+                if(numbers[j] - numbers[i] == target)
+                    count++;
+            }
+        }
+        return count;
+    } */
 }
