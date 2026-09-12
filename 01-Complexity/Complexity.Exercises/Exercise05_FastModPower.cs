da2f88881ff6c03e341b29a9b6479422a5d1777f
Jonathan Lopes <jdmlopes97@gmail.com>
2026-09-12 20:37:19 -0300

    solved exercise 5

--- 01-Complexity/Complexity.Exercises/Exercise05_FastModPower.cs
+++ 01-Complexity/Complexity.Exercises/Exercise05_FastModPower.cs
@@ -8,6 +8,19 @@ public static class FastModPower
 {
     public static long Pow(long baseValue, long exponent, long modulus)
     {
-        throw new NotImplementedException();
+        if(baseValue < 0 || exponent < 0 || modulus < 1 || modulus > int.MaxValue)
+            throw new ArgumentOutOfRangeException();
+        
+        long result = 1 % modulus;
+        baseValue %= modulus;
+        while(exponent > 0)
+        {
+            if(exponent % 2 == 1)
+                result = result * baseValue % modulus;
+
+            baseValue = baseValue * baseValue % modulus;
+            exponent /= 2;
+        }
+        return result;
     }
 }
