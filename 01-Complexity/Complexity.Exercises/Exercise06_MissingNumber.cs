e5fb9976d85002397ac1eb25c65c62dad725399f
Jonathan Lopes <jdmlopes97@gmail.com>
2026-09-12 21:09:10 -0300

    exercise 6 solved

--- 01-Complexity/Complexity.Exercises/Exercise06_MissingNumber.cs
+++ 01-Complexity/Complexity.Exercises/Exercise06_MissingNumber.cs
@@ -8,6 +8,34 @@ public static class MissingNumber
 {
     public static int Find(int[] numbers)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(numbers);
+
+        if(numbers.Length == 0)
+            return 0;
+        
+        long n = numbers.Length;
+        long expected = n * (n + 1) / 2;
+        long actual = 0;
+        foreach(int x in numbers)
+            actual += x;
+        return (int) (expected - actual);
+
     }
+
+    /* Solution using sort     
+    public static int Find(int[] numbers)
+    {
+        ArgumentNullException.ThrowIfNull(numbers);
+
+        if(numbers.Length == 0)
+            return 0;
+        numbers.Sort();
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            if(numbers[i] != i)
+                return i;
+        }
+        return numbers.Last() + 1;
+
+    } */
 }
