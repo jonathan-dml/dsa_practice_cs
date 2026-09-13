bdb0068677bcb98cd14142b0a8195e3394672d35
Jonathan Lopes <jdmlopes97@gmail.com>
2026-09-13 14:10:16 -0300

    2nd exercise solved

--- 02-Recursion/Recursion.Exercises/Exercise02_Fibonacci.cs
+++ 02-Recursion/Recursion.Exercises/Exercise02_Fibonacci.cs
@@ -6,13 +6,47 @@ namespace DsaPractice.Recursion;
 /// </summary>
 public static class Fibonacci
 {
+    private static Dictionary<int, long> storedValues = new();
     public static long Naive(int n)
     {
-        throw new NotImplementedException();
+        if (n < 0 || n > 92)
+            throw new ArgumentOutOfRangeException(nameof(n));
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return Naive(n - 1) + Naive(n - 2);
     }
 
     public static long Memoized(int n)
     {
-        throw new NotImplementedException();
+        if (n < 0 || n > 92)
+            throw new ArgumentOutOfRangeException(nameof(n));
+
+        if (storedValues.ContainsKey(n))
+        {
+            return storedValues[n];
+        }
+
+        if (n == 0)
+        {
+            storedValues[n] = 0;
+            return 0;
+        }
+
+        if (n == 1)
+        {
+            storedValues[n] = 1;
+            return 1;
+        }
+
+        storedValues[n] = Memoized(n - 1) + Memoized(n - 2);
+
+        return storedValues[n];
     }
+
+
+
+
 }
