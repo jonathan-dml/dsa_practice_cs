fa2bea6048f0317d69ae7716272fd685490d61b6
Jonathan Lopes <jdmlopes97@gmail.com>
2026-09-13 15:48:59 -0300

    4th exercise solved

--- 02-Recursion/Recursion.Exercises/Exercise04_ReverseString.cs
+++ 02-Recursion/Recursion.Exercises/Exercise04_ReverseString.cs
@@ -7,6 +7,47 @@ public static class StringReverser
 {
     public static string Reverse(string s)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(s);
+        
+        return new string(ReverseString(s.ToCharArray(), 0, s.Length - 1));
     }
+
+    private static char[] ReverseString(char[] s,int left,int right)
+    {
+        if(left >= right)
+        {
+            return s;
+        }
+        //tuple swap
+        (s[left], s[right]) = (s[right], s[left]);
+
+        return ReverseString(s,left+1,right-1);
+    }
+
+    /* Better iterative solution O(n/2) 
+    public static string Reverse(string s)
+    {
+        ArgumentNullException.ThrowIfNull(s);
+        int left = 0;
+        int right = s.Length - 1;
+        char[] reversedString = s.ToCharArray();
+        while(left < right)
+        {
+            char aux = reversedString[left];
+            reversedString[left] = reversedString[right];
+            reversedString[right] = aux;
+            left++;
+            right--;
+        }
+        return new string(reversedString);
+    } */
+    
+    /* Simple recursive solution O(n)
+    public static string Reverse(string s)
+    {
+        ArgumentNullException.ThrowIfNull(s);
+        if(s.Length <= 1)
+            return s;
+        return Reverse(s[1..]) + s[0];
+    } */
 }
